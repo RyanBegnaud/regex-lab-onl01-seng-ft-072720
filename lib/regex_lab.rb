5e05914b9234849c25b3033efa839c6513e4b02c
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-10 15:54:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,24 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+  word1 = word[0].scan /[aeiou]/
+  if word1.include? ("a"||"e"||"i"||"o"||"u") 
+    value = true 
+  elsif word1.include? ("A"||"E"||"I"||"O"||"U")
+    value = true 
+  else 
+    value = false 
+  end
+  value 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan (/\w+ing/)
 end
 
 def words_five_letters_long(text)
+  text.scan (/^\w{5}\s/)
+  binding.pry
+ 
 
 end
 
