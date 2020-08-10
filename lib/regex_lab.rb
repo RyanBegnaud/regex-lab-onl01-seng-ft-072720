2ce0a3dbabec6196f05c8e5f893742c13235331f
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-10 15:55:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,10 +14,14 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan (/\w+ing/)
 end
-
+  
 def words_five_letters_long(text)
-  text.scan (/^\w{5}\s/)
+  text = text.split
+  text.scan (/\w{5}\s/)
   binding.pry
+ 
+
+end
  
 
 end
